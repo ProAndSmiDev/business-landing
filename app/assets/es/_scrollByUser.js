d6db24cfd7f3d55dfa53d6d9488ee18230a1e1ec
AndSmi <andsmidev@gmail.com>
2021-06-01 18:22:32 +0300

    Fixed classes and starts to change active classes for menu by scroll

--- app/assets/es/_scrollByUser.js
+++ app/assets/es/_scrollByUser.js
@@ -9,12 +9,15 @@ document.addEventListener('DOMContentLoaded', () => {
     behavior: 'smooth',
   });
 
+  const scrollDistance = window.scrollY;
   const allLinks = document.querySelectorAll('a[href^="#"]');
   const header = document.querySelector(`${prefix}-header.js--scroll-by-user`);
   const mainBlock = document.querySelector(`${prefix}-page__main`);
   const isFixed = header.classList.contains(`${prefix.slice(1)}-header--is-fixed`);
   const isSticky = header.classList.contains(`${prefix.slice(1)}-header--is-sticky`);
   const headerHeight = (isFixed || isSticky) ? header.offsetHeight : 0;
+  const menuItem = document.querySelectorAll(`header ${prefix}-menu-list__item`);
+  const allSections = document.querySelectorAll('section');
 
   if (isFixed || isSticky) {
     mainBlock.style.marginTop = `${headerHeight}px`;
@@ -36,4 +39,20 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   }
+
+  // window.addEventListener('scroll', () => {
+  //   allSections.forEach((el, i) => {
+  //     if (el.offsetTop - header.clientHeight <= scrollDistance) {
+  //       console.log(el.offsetTop - header.clientHeight);
+
+  //       menuItem.forEach((link) => {
+  //         if (link.classList.contains(`${prefix.slice(1)}-menu-list__item--is-active`)) {
+  //           link.classList.remove(`${prefix.slice(1)}-menu-list__item--is-active`);
+  //         }
+  //       });
+
+  //       menuItem[i].classList.add(`${prefix.slice(1)}-menu-list__item--is-active`);
+  //     }
+  //   });
+  // });
 });
