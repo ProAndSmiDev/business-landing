0bf2ab30995e3ee4df17978d0f7c801ff7f5a1ce
AndSmi <andsmidev@gmail.com>
2021-06-02 17:55:29 +0300

    Need to fix some js code

--- app/assets/es/_menu.js
+++ app/assets/es/_menu.js
@@ -1,28 +1,64 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const clientSize = window.screen.width;
+/**
+ * Нужно оптимизировать и создать код для скролла страницы
+ * 1. Скролл по клику с изменением состояния пункта меню
+ * 2. Скролл страницы с изменением состояния пункта меню
+ */
+// document.addEventListener('DOMContentLoaded', () => {
+//   const clientSize = window.screen.width;
 
-  if (clientSize >= 1024) {
-    const menuLine = document.querySelector(`${prefix}-menu__line`);
-    const menuItem = document.querySelectorAll(`header ${prefix}-menu-list__item`);
+//   if (clientSize >= 1024) {
+//     const header = document.querySelector(`${prefix}-header.js--scroll-by-user`);
+//     const isFixed = header.classList.contains(`${prefix.slice(1)}-header--is-fixed`);
+//     const isSticky = header.classList.contains(`${prefix.slice(1)}-header--is-sticky`);
+//     const headerHeight = (isFixed || isSticky) ? header.offsetHeight : 0;
+//     const menuLine = document.querySelector(`${prefix}-menu__line`);
+//     const menuItem = document.querySelectorAll(`header ${prefix}-menu-list__item`);
+//     const allSections = document.querySelectorAll('section');
 
-    for (let i = 0; i < menuItem.length; i++) {
-      if (menuItem[i].classList.contains(`${prefix}-menu-list__item--is-active`)) {
-        menuLine.style.width = `${menuItem[i].offsetWidth - 15}px`;
-        menuLine.style.left = `${menuItem[i].offsetLeft}px`;
-      } else {
-        menuLine.style.width = `${menuItem[0].offsetWidth - 15}px`;
-        menuLine.style.left = `${menuItem[0].offsetLeft}px`;
-      }
+//     menuItem[0].classList.add(`${prefix}-menu-list__item--is-active`);
 
-      menuItem[i].addEventListener('mouseenter', () => {
-        menuLine.style.width = (i !== menuItem.length - 1) ? `${menuItem[i].offsetWidth - 15}px` : `${menuItem[i].offsetWidth}px`;
-        menuLine.style.left = `${menuItem[i].offsetLeft}px`;
-      });
+//     menuItem.forEach((el, i) => {
+//       if (el.classList.contains(`${prefix}-menu-list__item--is-active`)) {
+//         menuLine.style.width = `${el.offsetWidth - 15}px`;
+//         menuLine.style.left = `${el.offsetLeft}px`;
+//       } else {
+//         menuLine.style.width = `${menuItem[0].offsetWidth - 15}px`;
+//         menuLine.style.left = `${menuItem[0].offsetLeft}px`;
+//       }
 
-      menuItem[i].addEventListener('mouseleave', () => {
-        menuLine.style.width = `${menuItem[0].offsetWidth - 15}px`;
-        menuLine.style.left = '0';
-      });
-    }
-  }
-});
+//       el.addEventListener('mouseenter', () => {
+//         menuLine.style.width = (i !== menuItem.length - 1) ? `${el.offsetWidth - 15}px` : `${el.offsetWidth}px`;
+//         menuLine.style.left = `${el.offsetLeft}px`;
+//       });
+
+//       el.addEventListener('mouseleave', () => {
+//         menuLine.style.width = `${menuItem[0].offsetWidth - 15}px`;
+//         menuLine.style.left = '0';
+//       });
+
+//       el.addEventListener('click', () => {
+//         if (el.classList.contains(`${prefix.slice(1)}-menu-list__item--is-active`)) {
+//           el.classList.remove(`${prefix.slice(1)}-menu-list__item--is-active`);
+//         }
+
+//         menuItem[i].classList.add(`${prefix.slice(1)}-menu-list__item--is-active`);
+//       });
+//     });
+
+//     window.addEventListener('scroll', () => {
+//       const scrollDistance = window.scrollY;
+
+//       allSections.forEach((el, i) => {
+//         if (el.offsetTop - headerHeight <= scrollDistance) {
+//           menuItem.forEach((el) => {
+//             if (el.classList.contains(`${prefix.slice(1)}-menu-list__item--is-active`)) {
+//               el.classList.remove(`${prefix.slice(1)}-menu-list__item--is-active`);
+//             }
+//           });
+
+//           menuItem[i].classList.add(`${prefix.slice(1)}-menu-list__item--is-active`);
+//         }
+//       });
+//     });
+//   }
+// });
--- app/assets/es/_scrollByUser.js
+++ app/assets/es/_scrollByUser.js
@@ -1,58 +1,46 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const getOffsetPos = (blockPos, offsetByTop) => blockPos - offsetByTop;
-  const scrollToStart = () => document.body.scrollIntoView({
-    block: 'start',
-    behavior: 'smooth',
-  });
-  const scrollToBlock = (offset) => window.scrollBy({
-    top: offset,
-    behavior: 'smooth',
-  });
+/**
+ * Нужно оптимизировать код и переименовать файл
+ * 1. Создать плавный скролл по всем ссылкам-якорям
+ * 2. Переименовать файл в плавный скрол
+*/
 
-  const scrollDistance = window.scrollY;
-  const allLinks = document.querySelectorAll('a[href^="#"]');
-  const header = document.querySelector(`${prefix}-header.js--scroll-by-user`);
-  const mainBlock = document.querySelector(`${prefix}-page__main`);
-  const isFixed = header.classList.contains(`${prefix.slice(1)}-header--is-fixed`);
-  const isSticky = header.classList.contains(`${prefix.slice(1)}-header--is-sticky`);
-  const headerHeight = (isFixed || isSticky) ? header.offsetHeight : 0;
-  const menuItem = document.querySelectorAll(`header ${prefix}-menu-list__item`);
-  const allSections = document.querySelectorAll('section');
+// document.addEventListener('DOMContentLoaded', () => {
+//   const getOffsetPos = (blockPos, offsetByTop) => blockPos - offsetByTop;
+//   const scrollToStart = () => document.body.scrollIntoView({
+//     block: 'start',
+//     behavior: 'smooth',
+//   });
+//   const scrollToBlock = (offset) => window.scrollBy({
+//     top: offset,
+//     behavior: 'smooth',
+//   });
 
-  if (isFixed || isSticky) {
-    mainBlock.style.marginTop = `${headerHeight}px`;
-  }
 
-  for (let i = 0; i < allLinks.length; i++) {
-    allLinks[i].addEventListener('click', (e) => {
-      e.preventDefault();
+//   const allLinks = document.querySelectorAll('a[href^="#"]');
+//   const header = document.querySelector(`${prefix}-header.js--scroll-by-user`);
+//   const mainBlock = document.querySelector(`${prefix}-page__main`);
+//   const isFixed = header.classList.contains(`${prefix.slice(1)}-header--is-fixed`);
+//   const isSticky = header.classList.contains(`${prefix.slice(1)}-header--is-sticky`);
+//   const headerHeight = (isFixed || isSticky) ? header.offsetHeight : 0;
 
-      const blockID = allLinks[i].getAttribute('href');
+//   if (isFixed || isSticky) {
+//     mainBlock.style.marginTop = `${headerHeight}px`;
+//   }
 
-      if (blockID !== '#') {
-        const blockByID = document.querySelector(blockID);
-        const blockByIDPos = blockByID.getBoundingClientRect().top;
+//   for (let i = 0; i < allLinks.length; i++) {
+//     allLinks[i].addEventListener('click', (e) => {
+//       e.preventDefault();
 
-        scrollToBlock(getOffsetPos(blockByIDPos, headerHeight));
-      } else {
-        scrollToStart();
-      }
-    });
-  }
+//       const blockID = allLinks[i].getAttribute('href');
 
-  // window.addEventListener('scroll', () => {
-  //   allSections.forEach((el, i) => {
-  //     if (el.offsetTop - header.clientHeight <= scrollDistance) {
-  //       console.log(el.offsetTop - header.clientHeight);
+//       if (blockID !== '#') {
+//         const blockByID = document.querySelector(blockID);
+//         const blockByIDPos = blockByID.getBoundingClientRect().top;
 
-  //       menuItem.forEach((link) => {
-  //         if (link.classList.contains(`${prefix.slice(1)}-menu-list__item--is-active`)) {
-  //           link.classList.remove(`${prefix.slice(1)}-menu-list__item--is-active`);
-  //         }
-  //       });
-
-  //       menuItem[i].classList.add(`${prefix.slice(1)}-menu-list__item--is-active`);
-  //     }
-  //   });
-  // });
-});
+//         scrollToBlock(getOffsetPos(blockByIDPos, headerHeight));
+//       } else {
+//         scrollToStart();
+//       }
+//     });
+//   }
+// });
